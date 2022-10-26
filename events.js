b58dc7ca70a2c578f2a489fe9ea5cfb1a1b30a60
DIMANVAZ <DIMANVAZ04@GMAIL.COM>
2022-10-26 20:48:14 +0600

    feat events

---
+++ events.js
@@ -0,0 +1,21 @@
+const Emitter = require('events');
+const emitter = new Emitter();
+
+// слушатели
+const foo = (...fooArgs)=> {console.log(`foo: `, ...fooArgs);}
+const bar = (...barArgs)=> {console.log(`bar: `, ...barArgs);}
+const baz = (...bazArgs)=> {console.log(`baz: `, ...bazArgs);}
+
+emitter.on('event_1', foo);
+emitter.on('event_1', ()=>console.log('Arrow Lisnr'));
+emitter.once('event_1', bar);
+
+emitter.emit('event_1', 1, true, 'word');
+emitter.emit('event_1', 2, false, 'excel');
+
+emitter.removeListener('event_1', bar);
+emitter.emit('event_1', 3, true, 'poPo');
+emitter.emit('event_1', 4, false, 'visio');
+
+emitter.removeAllListeners('event_1');
+emitter.emit('event_1', 5, true, 'visio');
